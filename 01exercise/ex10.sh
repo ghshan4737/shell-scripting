b6a9e787a94beb970119923aba38a457ff334d46
yunshan <ghshan4737@gmail.com>
2022-09-24 22:56:21 +0800

    10 exercise done

---
+++ 01exercise/ex10.sh
@@ -0,0 +1,28 @@
+# 题目：编写一个脚本，接受一个资源路径作为参数。让脚本报告该资源是文件、目录或是其他类型。如果是目录，则以状态码1退出。如果是其他类型文件，以状态码2退出。
+
+echo "资源路径为$1"
+RES=$1
+if [ -f $RES ]
+    then 
+    echo "该资源是一个文件"
+elif [ -d $RES ]
+    then
+    echo "该资源是一个目录"
+    exit 1
+else
+    echo "该资源是一个其他类型文件"
+    exit 2
+fi
+
+# 输出结果如下：
+
+# $ ./ex10.sh ex9.sh
+# 资源路径为ex9.sh
+# 该资源是一个文件
+
+# $ ./ex10.sh .
+# 资源路径为.
+# 该资源是一个目录
+
+# 本题练习了分支语句中使用不同退出状态码的方法。
+# This problem exercises the use of different exit status codes in branch statements.
--- 01exercise/ex8.sh
+++ 01exercise/ex8.sh
@@ -1,24 +1,50 @@
 # 题目：在ex7.sh的基础上，修改脚本使其能够接收不限个数的参数。
 # Modify the previous script to accept an unlimited number of files and directories as arguments.
 
-no=$#
-count=0
-echo $no
-echo $count
-while [ $count -lt $no ]
-do
-    echo $count
-    echo $[$count]
-    if [ -f $[$count] ]
+#!/bin/bash
+RESOURCES=$@
+count=1
+for RS in $RESOURCES    
+    do
+    if [ -f $RS ]
         then
-            echo "这是一个文件"
-    elif [ -d $[$count] ]
+            echo "第$count个参数是一个文件"
+    elif [ -d $RS ]
         then
-            echo "这是一个目录"
+            echo "第$count个参数是一个目录"
     else
         echo "这是一个其他类型的文件"
     fi
 
-    ls -l $count
-    count='expr $count + 1'
-done
+    ls -l $RS
+    count=$(expr $count + 1)
+    # count=`expr $count + 1`
+    # let count+=1
+    # ((count++))
+    # count=$[$count+1]
+    # count=$(($count + 1))
+    echo 
+    done
+
+# 输出结果如下：
+# $ ./ex8.sh ex6.sh READEME .
+# 第1个参数是一个文件
+# -rwxrwxr-x 1 yunshan yunshan 1078  9月 24 21:14 ex6.sh
+
+# 第2个参数是一个文件
+# -rw-rw-rw- 1 yunshan yunshan 117  9月 24 19:42 READEME
+
+# 第3个参数是一个目录
+# 总用量 36
+# -rwxrwxr-x 1 yunshan yunshan  193  9月 24 19:42 ex1.sh
+# -rwxrwxr-x 1 yunshan yunshan  213  9月 24 19:42 ex2.sh
+# -rwxrwxr-x 1 yunshan yunshan  434  9月 24 20:56 ex3.sh
+# -rwxrwxr-x 1 yunshan yunshan  982  9月 24 20:54 ex4.sh
+# -rwxrwxr-x 1 yunshan yunshan  650  9月 24 20:54 ex5.sh
+# -rwxrwxr-x 1 yunshan yunshan 1078  9月 24 21:14 ex6.sh
+# -rwxrwxr-x 1 yunshan yunshan  687  9月 24 21:24 ex7.sh
+# -rwxrwxr-x 1 yunshan yunshan  670  9月 24 22:20 ex8.sh
+# -rw-rw-rw- 1 yunshan yunshan  117  9月 24 19:42 READEME
+
+# 本题练习了多参数shell脚本遍历参数的方法。同时在脚本内练习了变量自增的几种写法。对输出结果进行了美化。
+# This question exercises the method of traversing parameters in a multi-parameter shell script. At the same time, several ways of writing variable auto-increment are practiced in the script. The output result has been beautified.
